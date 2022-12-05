df705b4e2cc458219478d2912a575218fecc1c8a
Piotr Szczerba <74144578+Siraz0@users.noreply.github.com>
2022-12-05 15:21:06 +0100

    VM Creation feature added

--- initial_proxmox_setup.sh
+++ initial_proxmox_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 proxmox_init () {
-    echo "============== PROXMOX setup ======================"
+    echo "======================== Proxmox Setup =========================="
     echo "Settings sources list..."
-    #rm -rf "/etc/apt/sources.list"
+    rm -rf "/etc/apt/sources.list"
     echo "deb http://ftp.debian.org/debian bullseye main contrib
 deb http://ftp.debian.org/debian bullseye-updates main contrib
 # security updates
@@ -17,10 +17,37 @@ deb http://download.proxmox.com/debian/pve bullseye pve-no-subscription" > /etc/
     echo "Running update&dist-upgrade"
     apt-get update -y
     apt dist-upgrade -y
-    echo "Done."
-    echo "-----------------------------------------------------------"
-    echo "|             Proxmox init setup done !                   |"
-    echo "-----------------------------------------------------------"
+    echo "----------------------------------------------------------------"
+    echo "|                  Proxmox init setup done !                   |"
+    echo "----------------------------------------------------------------"
+}
+
+create_devops_vm () {
+    echo "========================== VM setup ============================="
+    echo "============== Downloading ISO and dependencies ================="
+    echo "Downloading ubuntu-20.04-server iso..."
+    wget -P /var/lib/vz/template/iso "https://releases.ubuntu.com/20.04.5/ubuntu-20.04.5-live-server-amd64.iso"
+    echo "Download complete."
+    echo "Settings DevOps machine..."
+    echo "Please provide VM details:"
+    read -p "VM Name: " vm_name
+    read -p "VM ID: " vm_id
+    read -p "VM Cores: " vm_cores
+    read -p "VM Memory: " vm_mem
+    qm create $vm_id --cdrom local:iso/ubuntu-20.04.5-live-server-amd64.iso \
+  --name $vm_name --ostype l26 \
+  --cpu cputype=host --cores $vm_cores \
+  --memory $vm_mem  \
+  --net0 bridge=vmbr0,virtio \
+  --bootdisk scsi0 --scsihw virtio-scsi-pci --scsi0 file=local-lvm:32
+    sleep 3
+    qm set $vm_id -onboot 1 -agent 1 -autostart 1
+    echo "----------------------------------------------------------------"
+    echo "|                  VM: $vm_name - created !                    |"
+    echo "----------------------------------------------------------------"
+}
+
+host_reboot () {
     echo "Rebooting machine in"
     echo "3..."
     sleep 1
@@ -30,18 +57,6 @@ deb http://download.proxmox.com/debian/pve bullseye pve-no-subscription" > /etc/
     sleep 1
     echo "CYA!"
     reboot
-}
-
-create_devops_vm () {
-    echo "============== VM setup ================="
-    echo "Downloading ubuntu-20.04-server iso..."
-    wget -P /var/lib/vz/template/iso "https://releases.ubuntu.com/20.04.5/ubuntu-20.04.5-live-server-amd64.iso"
-    echo "Download complete."
-    echo "Downloading packer..."
-    apt-get install packer -y
-    echo "Downloading git..."
-    apt-get install git -y
-    echo "Download complete."
 }
 
 echo "-----------------------------------------------------------"
@@ -59,12 +74,14 @@ read -p "Option: " option
 case $option in
     1)
     #Proxmox init setup with devops VM
-    create_devops_vm
     proxmox_init
+    create_devops_vm
+    host_reboot
     ;;
     2)
     #Only proxmox init setup
     proxmox_init
+    host_reboot
     ;;
     *)
     echo "Please choose 1 or 2. Exitting.."
